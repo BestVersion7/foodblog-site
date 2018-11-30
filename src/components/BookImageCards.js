238cfa3eb8e977f74802f9733d68758ec984f0b0
BestVersion7 <earthsmoonandspoon@yahoo.com>
2018-11-30 14:16:36 -0500

    Change book text

--- src/components/BookImageCards.js
+++ src/components/BookImageCards.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
-const books = {
-	bountiful: require('../assets/bountiful.jpg'),
-	italian: require('../assets/italian.jpg'),
-	lappetito: require('../assets/lappetito.jpg'),
-	loverealfood: require('../assets/loverealfood.jpg')
-}
-
 const BookImageCards = () => {
 	return (
 	 	<div style={styles.container}>
-		    <h2> Cookbooks  </h2>
+		    <h2> Develop Your Cooking Technique!  </h2>
 		    <div style = {styles.cardcontainer}>
-		       <img style={styles.imgbook} src={books.bountiful} alt="bountiful" />
-		       <img style={styles.imgbook} src={books.italian} alt="italian" />
-		       <img style={styles.imgbook} src={books.lappetito} alt = "lappetito"/>
-		       <img style={styles.imgbook} src={books.loverealfood} alt="loverealfood" />
-		      <a style = {{marginTop:'20%'}} href="https://google.com">See More></a>
+		       <img style={styles.imgbook} src="https://books.google.com/books/content/images/frontcover/IGSwLLETenMC?fife=w200-h300" alt="Gordon Ramsay" />
+		       <img style={styles.imgbook} src="https://books.google.com/books/content/images/frontcover/h1z5lgji7GMC?fife=w200-h300" alt="Gordon Ramsay" />
+		       <img style={styles.imgbook} src="https://books.google.com/books/content/images/frontcover/5qJKDwAAQBAJ?fife=w200-h300" alt="Gordon Ramsay" />
+		       <img style={styles.imgbook} src="https://images-na.ssl-images-amazon.com/images/I/51l3mBqNdYL._SX258_BO1,204,203,200_.jpg" alt="graham" />
+		      <a style = {{marginTop:'10%'}} href="https://google.com"> See More > </a>
 			  </div>
 		</div>
 	)
@@ -29,7 +22,7 @@ const styles = {
 
 	cardcontainer: {
 		display:'grid',
-		gridTemplate: '300px/220px 220px 220px 220px auto',
+		gridTemplate: '300px/220px 220px 220px 220px 1fr',
     overflow: 'auto',
 		gridGap: '1.5%'
 	},
